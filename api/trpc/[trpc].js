fbcb558d6b93f5966fa4657340284e9eda238742
@PovertyPimpSlayer <TainoRican2n@gmail.com>
2026-06-18 11:42:38 +0800

    fix: switch tRPC handler to nodejs runtime for supabase compat

--- api/trpc/[trpc].js
+++ api/trpc/[trpc].js
@@ -1,10 +1,11 @@
+import { createServer } from '@trpc/server/adapters/standalone';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
-import { appRouter } from '../../src/server/routers/_app';
-import { createContext } from '../../src/server/context';
+import { appRouter } from '../../src/server/routers/_app.js';
+import { createContext } from '../../src/server/context.js';
 
-export const config = { runtime: 'edge' };
+export const config = { runtime: 'nodejs' };
 
-export default function handler(req) {
+export default function handler(req, res) {
   return fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
